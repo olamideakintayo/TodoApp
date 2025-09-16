1f7b336c2099c278e746f66d652134e1620a47d6
olamideakintayo <divineolamide977@gmail.com>
2025-09-16 11:44:07 +0100

    Added the JWT Auth filter, CustomUserDetailsService for DB verification and Also add SPring Security config to use the JWT impl

---
+++ src/main/java/org/kashcode/todoapp/services/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package org.kashcode.todoapp.services;
+
+import org.kashcode.todoapp.data.models.User;
+import org.kashcode.todoapp.data.repositories.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(usernameOrEmail)
+                .or(() -> userRepository.findByEmail(usernameOrEmail))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + usernameOrEmail));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
