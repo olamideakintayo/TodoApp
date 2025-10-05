e89c0c9cc8a45bcb1d66ccff40e4a10711f58db8
olamideakintayo <divineolamide977@gmail.com>
2025-10-05 21:06:44 +0100

    jwt refactor

--- src/main/java/org/kashcode/todoapp/security/JwtAuthenticationFilter.java
+++ src/main/java/org/kashcode/todoapp/security/JwtAuthenticationFilter.java
@@ -21,6 +21,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
+
     public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
