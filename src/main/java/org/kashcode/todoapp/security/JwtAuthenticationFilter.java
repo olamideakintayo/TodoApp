76309f52114a7594dc8db3cc26808fde02432350
olamideakintayo <divineolamide977@gmail.com>
2025-10-08 10:23:48 +0100

    added cors config

--- src/main/java/org/kashcode/todoapp/security/JwtAuthenticationFilter.java
+++ src/main/java/org/kashcode/todoapp/security/JwtAuthenticationFilter.java
@@ -21,7 +21,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
-
     public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
